eabc56af07e309fe64a6a1bbadab092216f3eba3
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2025-06-17 12:36:07 +0300

    [IMP] If invoice and delivery goes to same country, use invoice address VAT

--- l10n_fi_intrastat_product/__manifest__.py
+++ l10n_fi_intrastat_product/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Intrastat: export Finnish Intrastat CSV",
     "summary": "Finnish Intrastat CSV-report for Customs (Tulli)",
-    "version": "17.0.1.0.2",
+    "version": "17.0.1.0.3",
     "category": "CRM",
     "website": "https://github.com/tawasta/intrastat-extrastat",
     "author": "Futural",
